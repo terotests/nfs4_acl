8a0565929b3054622dcd6acade3020302bde62ee
Tero Tolonen <digikansio@gmail.com>
2015-06-22 22:13:34 +0300

    "commit message"

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# nfs4_acl
+# nfs4_acl - NFSv4 Access Control Lists
+
+JavaScript library implementing NFSv4 Access Control Lists basic ACL behaviour.
+
+The library does not create the storage for the ACL, it will only check whether a specified access is granted for a user or not.
+
+The library tries to be a very conservative for the memory allocations, it avoids splitting strings or doing things that require allocation or de-allocating memory to avoid V8 garbage collection starting at the node.js server.
+
+# Initializing the ACL
+
+```javascript
+
+var file = "A::OWNER@:rwatTnNcCy\n"+
+"A::alice@nfsdomain.org:rxtncy\n"+
+"A::bob@nfsdomain.org:rwadtTnNcCy\n"+
+"A:g:GROUP@:rtncy\n"+
+"D:g:GROUP@:waxTC\n"+
+"A::EVERYONE@:rtncy\n"+
+"D::EVERYONE@:waxTC\n";
+
+var acl = nfs4_acl( file );
+
+```
+
+# Checking for permission flags
+
+For list of flags check: http://linux.die.net/man/5/nfs4_acl
+
+## Checking for a single permission
+
+```javascript
+ if( acl.find("alice@nfsdomain.org", "", "r") ) {
+    console.log("Alice has read access");
+ }
+```
+
+## Checking multiple permissions
+
+```javascript
+ if( acl.find("alice@nfsdomain.org", "", "r") ) {
+    console.log("Alice has read access");
+ }
+
+# Alternative approaches
+
+To consume less `node.js/io.js` memory the class could allocate ArrayBuffer for the ACL based on the reference implementation.
+
+
+# License.
+
+MIT
+
+
+
+
+
+
+
+
---
+++ release/nfs4_acl.js
@@ -0,0 +1,154 @@
+var nfs4_acl_prototype = function() {
+  'use strict';;
+  (function(_myTrait_) {
+    _myTrait_.find = function(username, rolename, rule) {
+
+      var i = 0,
+        line_i = 0,
+        type_i = 0;
+      length = this._acl.length;
+
+      var type, flags, principal, permissions, flag, bGroup = false,
+        uni = 0,
+        uni_match = false,
+        uni_failed = false,
+        ignore_line = false;
+
+      /*
+           A::OWNER@:rwatTnNcCy
+           A::alice@nfsdomain.org:rxtncy
+           A::bob@nfsdomain.org:rwadtTnNcCy
+           A:g:GROUP@:rtncy
+           D:g:GROUP@:waxTC
+           */
+
+      while (i < length) {
+
+        if (this._acl.charAt(i) == ":") {
+          line_i++;
+          type_i++;
+          i++;
+          continue;
+        }
+        if (this._acl.charAt(i) == "\n") {
+          line_i = 0;
+          type_i = 0;
+          i++;
+          continue;
+        }
+
+        if (line_i == 0) {
+          ignore_line = false;
+          type = this._acl.charAt(i);
+          line_i++;
+          i++;
+          uni_match = false;
+          uni_failed = false;
+          uni = 0;
+          bGroup = false;
+          continue;
+        }
+        if (type_i == 1) {
+          flag = this._acl.charAt(i);
+          if (flag == "g") bGroup = true;
+          if (flag == "i") ignore_line = true;
+          line_i++;
+          i++;
+          continue;
+        }
+        if (type_i == 2) {
+          if (bGroup) {
+            if (this._acl.charAt(i) == rolename.charAt(uni++)) {
+              uni_match = true;
+            } else {
+              uni_match = false;
+              uni_failed = true;
+            }
+          } else {
+            if (this._acl.charAt(i) == username.charAt(uni++)) {
+              uni_match = true;
+            } else {
+              uni_match = false;
+              uni_failed = true;
+            }
+          }
+          line_i++;
+          i++;
+          continue;
+        }
+        if (type_i == 3) {
+          if (uni_match && !uni_failed && !ignore_line) {
+            if (rule.indexOf(this._acl.charAt(i)) >= 0) {
+              if (type == "A") return true;
+              if (type == "D") return false;
+            }
+          }
+          line_i++;
+          i++;
+          continue;
+        }
+        line_i++;
+        i++;
+      }
+      return false;
+
+    }
+    if (_myTrait_.__traitInit && !_myTrait_.hasOwnProperty("__traitInit"))
+      _myTrait_.__traitInit = _myTrait_.__traitInit.slice();
+    if (!_myTrait_.__traitInit) _myTrait_.__traitInit = []
+    _myTrait_.__traitInit.push(function(aclFile) {
+      /*
+A::OWNER@:rwatTnNcCy
+A::alice@nfsdomain.org:rxtncy
+A::bob@nfsdomain.org:rwadtTnNcCy
+A:g:GROUP@:rtncy
+D:g:GROUP@:waxTC
+A::EVERYONE@:rtncy
+D::EVERYONE@:waxTC
+*/
+
+      this._acl = aclFile;
+
+      // type:flags:principal:permissions
+      // Types : A, D, U, L
+
+      /*
+A principal is either a named user (e.g., 'myuser@nfsdomain.org') or group (provided the group flag is also set), 
+or one of three special principals: 'OWNER@', 'GROUP@', and 'EVERYONE@', which are, respectively, analogous to the 
+POSIX user/group/other distinctions used in, e.g., chmod(1).
+*/
+    });
+  }(this));
+}
+var nfs4_acl = function(a, b, c, d, e, f, g, h) {
+  if (this instanceof nfs4_acl) {
+    var args = [a, b, c, d, e, f, g, h];
+    if (this.__factoryClass) {
+      var m = this;
+      var res;
+      this.__factoryClass.forEach(function(initF) {
+        res = initF.apply(m, args);
+      });
+      if (Object.prototype.toString.call(res) == '[object Function]') {
+        if (res._classInfo.name != nfs4_acl._classInfo.name) return new res(a, b, c, d, e, f, g, h);
+      } else {
+        if (res) return res;
+      }
+    }
+    if (this.__traitInit) {
+      var m = this;
+      this.__traitInit.forEach(function(initF) {
+        initF.apply(m, args);
+      })
+    } else {
+      if (typeof this.init == 'function')
+        this.init.apply(this, args);
+    }
+  } else return new nfs4_acl(a, b, c, d, e, f, g, h);
+};
+nfs4_acl._classInfo = {
+  name: 'nfs4_acl'
+};
+nfs4_acl.prototype = new nfs4_acl_prototype();
+if (typeof(window) != 'undefined') window['nfs4_acl'] = nfs4_acl;
+if (typeof(window) != 'undefined') window['nfs4_acl_prototype'] = nfs4_acl_prototype;
